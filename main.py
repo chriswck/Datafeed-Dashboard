7c17e08b74122ca22955a7e5603a17b91a0fd0d6
Christopher Wong <chriswck@hotmail.com>
2021-02-02 13:53:05 +0800

    Add typer cli

--- main.py
+++ main.py
@@ -12,29 +12,36 @@ import datafeeddashboard.GenericDashboardService as qs
 import json
 import os
 import sys
+import typer
 
 # Load other necessary S3 and DB credentials/variables into os module
 with open("envvars.json") as envVarFile:
     envVars = json.load(envVarFile)
     os.environ.update(envVars)
 
-def main(cli_args):
+# Explicitly create typer.Typer app
+app = typer.Typer()
+
+@app.command()
+def updateDashboard(sheet: str = typer.Argument(None,
+                                                help='Acceptable values: \
+                                                LastFileReceipt, \
+                                                LastAccountUpdate, \
+                                                ReconcilliationBreaks.')):
     gsheet = qs.gSheetServiceConnection()
     dispatch_map = {'LastFileReceipt': qs.updateLastFileReceipt,
                     'LastAccountUpdate': qs.updateLastAccountUpdate,
                     'ReconciliationBreaks': qs.updateReconciliationBreaks,
                     }
-    if len(cli_args) == 0:
-        for k,v in dispatch_map.items():
-            eval("v(gsheet)")
+    if sheet is None:
+        for k in dispatch_map.keys():
+            dispatch_map[k](gsheet)
     else:
-        if len(set(cli_args) - set(dispatch_map.keys())) > 0:
-            raise KeyError('{} invalid. Only accept arguments: {}'.format(\
-                list(set(cli_args) - set(dispatch_map.keys())), list(dispatch_map.keys())))
-        else:
-            for arg in cli_args:
-                eval("dispatch_map[arg](gsheet)")
+        dispatch_map[sheet](gsheet)
 
+@app.command()
+def triggerAlerts():
+    return 'This is work in progress'
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    app()
